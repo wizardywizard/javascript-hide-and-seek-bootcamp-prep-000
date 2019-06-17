20d6d1ad2c87df2470dfdd9860a052169161c9f5
David Little <littledavid175@gmail.com>
2019-06-17 17:30:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function deepestChild(){
    return lis3
 }
 function increaseRankBy(n){
-  var lis4=document.querySelectorAll('ul.ranked-class ').parseInt(4)
+  var lis4=document.querySelectorAll('ul.ranked-class li li li').parseInt(4)
   return lis4
 }
  
