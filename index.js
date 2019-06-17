1ce3a5d2c7e628f8efe9339ad2682f290e187db1
David Little <littledavid175@gmail.com>
2019-06-17 18:21:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function getFirstSelector(selector){
+  const lis= document.querySelector(selector)
+  return lis
+}
+function nestedTarget(){
+  var lis2=document.querySelector('#nested .target')
+  return lis2
+}
+function deepestChild(){
+   var lis3= document.querySelector('div#grand-node div div div div ')
+   return lis3
+}
+function increaseRankBy(n){
+  var lis4= document.querySelectorAll('ul.ranked-list li')
+  for (let i = 0; i < lis4.length; i++) {
+  lis4[i].innerHTML = parseInt(lis4[i].innerHTML) + n
+  }
+}
+ 
